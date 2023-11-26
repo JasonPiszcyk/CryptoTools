d0458c8b37490d06326c81c4c096bdfd50f80dd5
JasonPiszcyk <64060251+JasonPiszcyk@users.noreply.github.com>
2023-11-26 20:50:34 +1030

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,4 @@
+# CryptoTools
+Copyright (c) 2023 Jason Piszcyk
+
+Tools to be used for cryptographic functions.
---
+++ src/crypto_tools/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+* __init__.py
+*
+* Copyright (c) 2023 Iocane Pty Ltd
+*
+* @author: Jason Piszcyk
+*
+* Module initialisation
+*
+'''
+__all__ = [ "ApplicationConfig", "Config" ]
+
+from .application_config import ApplicationConfig
+from .application_config import Config
